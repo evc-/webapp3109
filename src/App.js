56de2d6385b1cee5001c892b61d3ee55f35c4b43
evelyn <evelyncranston@Live.ca>
2018-02-27 13:21:11 -0800

    Updated from last week

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import mySocket from "socket.io-client";
+import Rooms from './comp/Rooms';
 
 class App extends Component {
     
@@ -10,14 +11,17 @@ class App extends Component {
             myImg: require("./pic1.png"),
             myImg2: require("./pic2.png"),
             allUsers:[],
-            myId:null
+            myId:null,
+            showDisplay: false,
+            stickers:[]
         }
         
         this.handleImage = this.handleImage.bind(this);
+        this.handleDisplay = this.handleDisplay.bind(this);
     }
     
     componentDidMount(){
-        this.socket = mySocket("https://webapp3109.herokuapp.com/");
+        this.socket = mySocket("http://localhost:10000/");
         
         this.socket.on("userjoined", (data)=>{
             this.setState({
@@ -28,7 +32,40 @@ class App extends Component {
         this.socket.on("yourid", (data)=>{
             this.setState({
                 myId: data
+            });
+            
+            this.refs.theDisplay.addEventListener("mousemove", (ev)=>{
+            if(this.state.myId === null){
+            //FAIL
+                return false;
+            }
+            
+            this.refs["u"+this.state.myId].style.left = ev.pageX + "px";
+            this.refs["u"+this.state.myId].style.top = ev.pageY + "px";
+
+            
+            this.socket.emit("mymove", {
+                x:ev.pageX,
+                y:ev.pageY,
+                id: this.state.myId,
+                src:this.refs["u"+this.state.myId].src
             })
+
+        });
+            
+            this.refs.theDisplay.addEventListener("click",(ev)=>{
+                this.socket.emit("stick", {
+                    x: ev.pageX,
+                    y: ev.pageY,
+                    src:this.refs["u"+this.state.myId].src
+                });
+            });
+        });
+        
+        this.socket.on("newsticker", (data)=>{
+            this.setState({
+                stickers:data
+            });
         });
         
         this.socket.on("newmove", (data)=>{
@@ -37,6 +74,7 @@ class App extends Component {
             this.refs["u"+data.id].src = data.src;
         });
         
+        /*
         this.refs.theDisplay.addEventListener("mousemove", (ev)=>{
             if(this.state.myId === null){
             //FAIL
@@ -55,10 +93,19 @@ class App extends Component {
             })
 
         });
+        */
     }
     
     handleImage(evt){
         this.refs["u"+this.state.myId].src = evt.target.src;
+    }
+    
+    handleDisplay(roomString){
+        this.setState({
+            showDisplay:true
+        });
+        
+        this.socket.emit("joinroom", roomString);
     }
     
   render() {
@@ -69,21 +116,42 @@ class App extends Component {
           )
       });
       
+      var allstickers = this.state.stickers.map((obj, i)=>{
+          var mstyle = {left:obj.x, top:obj.y};
+          return (
+              <img style={mstyle} key={i} src={obj.src} height={50}
+              className="allImgs" />
+          )
+      })
+      
+    var comp = null;
+      
+      if (this.state.showDisplay === false){
+          comp = <Rooms handleDisplay={this.handleDisplay} />
+      } else {
+          (
+                comp = 
+                    <div>
+                <div ref="theDisplay" id="display">
+                {allImgs}
+                {allstickers}
+                    <img ref="myImg" id="myImg" src={this.state.myImg} height={100} />
+                </div>
+
+                <div id="controls">
+
+                {this.state.myId}
+                <img src={this.state.myImg} height={50} onClick={this.handleImage} />
+                <img src={this.state.myImg2} height={50} onClick={this.handleImage} />
+                </div>
+                </div>
+              )
+      }
+      
+      
     return (
       <div className="App">
-        
-        <div ref="theDisplay" id="display">
-        {allImgs}
-            <img ref="myImg" id="myImg" src={this.state.myImg} height={100} />
-        </div>
-        
-        <div id="controls">
-        
-        {this.state.myId}
-        <img src={this.state.myImg} height={50} onClick={this.handleImage} />
-        <img src={this.state.myImg2} height={50} onClick={this.handleImage} />
-        </div>
-        
+        {comp}
       </div>
     );
   }
